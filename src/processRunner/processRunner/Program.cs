389756172e89b447a5da79d04fc85a5521e35087
mihai.buzgau <mihai.buzgau@uhurusoftware.com>
2014-08-11 12:43:26 +0300

    initial commit

---
+++ src/processRunner/processRunner/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace processRunner
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            if (Environment.UserInteractive)
+            {
+                var svc = new ProcessRunner();
+                svc.Start();
+                Console.ReadKey();
+                svc.Stop();
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+			    { 
+				    new ProcessRunner() 
+			    };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
---
+++ src/processRunner/processRunner/Service1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace processRunner
+{
+    public partial class ProcessRunner : ServiceBase
+    {
+        public ProcessRunner()
+        {
+            InitializeComponent();
+        }
+
+        internal void Start()
+        {
+
+            string curDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Environment.CurrentDirectory = curDir;
+
+
+            var startInfo = new ProcessStartInfo("run.cmd");
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+
+            var process = new Process();
+            process.StartInfo = startInfo;
+
+            
+            process.EnableRaisingEvents = true;
+            process.ErrorDataReceived += new DataReceivedEventHandler(process_ErrorDataReceived);
+            process.OutputDataReceived += new DataReceivedEventHandler(process_OutputDataReceived);
+            process.Exited += new EventHandler(process_Exited);
+
+            this.runningProcess = process;
+            this.runningProcess.Start();
+        }
+
+        protected Process runningProcess;
+
+        protected override void OnStart(string[] args)
+        {
+            this.Start();            
+        }
+
+        void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            File.AppendAllText("run.out", e.Data);
+        }
+
+        void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            File.AppendAllText("run.err", e.Data);
+        }
+
+        void process_Exited(object sender, EventArgs e)
+        {
+            Environment.Exit(this.runningProcess.ExitCode);
+        }
+
+        protected override void OnStop()
+        {
+            if (this.runningProcess != null && !this.runningProcess.HasExited)
+            {
+                try
+                {
+                    this.runningProcess.Kill();
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
